78ba31d040bb21b35e541d45e81495337f23fede
Jay Paulson <paulsjv@gmail.com>
2017-05-14 10:00:08 -0500

    finishing assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  styles: [`
+    .toggle-text { color: white; }
+  `]
 })
 export class AppComponent {
+  toggle = true;
+  toggleClicks = [];
 
+  onToggle() {
+    this.toggleClicks.push(this.toggleClicks.length + 1);
+    this.toggle = !this.toggle;
+  }
 }
